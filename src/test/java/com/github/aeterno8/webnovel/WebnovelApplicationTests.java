7db1777bca01594879e78fff0b0749c5a2cb7bc4
Aleksa Savic <savicaleksa.kg@gmail.com>
2024-10-05 15:16:38 +0200

    Initial commit

---
+++ src/test/java/com/github/aeterno8/webnovel/WebnovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aeterno8.webnovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebnovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
